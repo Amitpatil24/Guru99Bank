d5750c532ba304deb68b66000d1c82de49775fe4
AMIT <amit.tester.24@gmail.com>
2023-02-27 19:40:19 +0530

    Adding New Changes

--- src/test/java/Runner/TestRun.java
+++ src/test/java/Runner/TestRun.java
@@ -7,11 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-         features = "src/test/java/features/DatatableReg.feature",   // path of the feature file
+         features = "src/test/java/features/DataDrivenWithMap.feature",   // path of the feature file
          glue = {"step_definition"},                 // path of the step definition file
          monochrome = true,              // display the console output in proper readable format
          plugin ={"pretty","html:test-output.html","json:json_output/cucumber.json","junit:junit_xml/cucumber.xml"},   // to generate different types of report
-         dryRun = false           // it is used to correct mapping between feature file and step definition file
+         dryRun = false          // it is used to correct mapping between feature file and step definition file
 
         //  strict = true -- it will check if any step is not defined in step definition file
 )
